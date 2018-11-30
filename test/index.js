b21aade620e3e8ef1564e9c7ef73e6b494300c6f
cjihrig <cjihrig@gmail.com>
2018-11-30 00:47:44 -0500

    send back original error message on 500

--- test/index.js
+++ test/index.js
@@ -22,4 +22,55 @@ describe('Developer', () => {
         const server = Hapi.server();
         await server.register(Developer);
     });
+
+    it('returns error message in tact', async () => {
+
+        const server = await internals.prepareServer();
+        const res = await server.inject('/throws');
+
+        expect(res.result).to.equal({
+            statusCode: 500,
+            error: 'Internal Server Error',
+            message: 'test error'
+        });
+    });
+
+    it('does not modify non-errors', async () => {
+
+        const server = await internals.prepareServer();
+        const res = await server.inject('/success');
+
+        expect(res.result).to.equal('success');
+    });
 });
+
+
+internals.prepareServer = async function () {
+
+    const server = Hapi.server();
+
+    await server.register({
+        plugin: Developer
+    });
+
+    server.route([
+        {
+            method: 'GET',
+            path: '/success',
+            handler: (request) => {
+
+                return 'success';
+            }
+        },
+        {
+            method: 'GET',
+            path: '/throws',
+            handler: (request) => {
+
+                throw new Error('test error');
+            }
+        }
+    ]);
+
+    return server;
+};
